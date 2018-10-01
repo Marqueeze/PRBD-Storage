73d38b873d31a08c67193cfb512a1aa5acea810a
Dmitry <naumenko.dmitry19@gmail.com>
2018-10-01 19:14:37 +0300

    Done all instances. Done Association tables. It is nearly working

--- main.py
+++ main.py
@@ -5,25 +5,69 @@ from __init__ import app, db
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    c = Client(name='Ochen sex')
+    return str(Print_DB())
+
+
+@app.route('/Client', methods=['GET', 'POST'])
+def client():
+    # c = Client(name='Дмитрий Иванович Соломатин')
+    w = Ware(name="Классы", price=0.01)
+    # o = Operation(id_Client=1, operation_type='+')
+    # op_w = WareOperation(ware_count=15)
+    # op_w.wares = w
+    # o.op_ware.append(op_w)
+    # db.session.add(c)
+    # db.session.add(w)
+    # db.session.add(o)
+    st_w = WareStorage(ware_count=15)
+    st_w.wares = w
+    s = Storage.query.get(1)
+    s.st_ware.append(st_w)
+    db.session.add(s)
+    db.session.commit()
+    return 'Client added'
+
+
+@app.route('/Ware', methods=['GET', 'POST'])
+def ware():
+    c = Ware(name="Классы", price=0.01)
     db.session.add(c)
     db.session.commit()
-    return 'Test'
+    return 'Ware added'
+
+
+@app.route('/Storage', methods=['GET', 'POST'])
+def storage():
+    c = Storage(phone_number='88005553535', address='Улица Пушкина Дом Колотушкина')
+    # c.wares.append(Ware.query.filter_by(name='Классы').first())
+    db.session.add(c)
+    db.session.commit()
+    return "Storage added"
+
+
+@app.route('/Operation', methods=['GET', 'POST'])
+def operation():
+    c = Operation(id_Client = 1, operation_type = '+')
+    op_w = WareOperation(ware_count=15)
+    op_w.wares = Ware.query.filter_by(name="Классы").first()
+    c.wares.append(op_w)
+    db.session.commit()
+    return 'Operation added'
 
 
 def Clear_DB():
-    for t in [Client, Storage, Operation, Ware]:
+    for t in [Client, Storage, Operation, Ware, WareOperation]:
         for t1 in t.query.all():
             db.session.delete(t1)
     db.session.commit()
 
 
 def Print_DB():
-    for t in [Client, Storage, Operation, Ware]:
-        print(t.query.all())
-
+    for t in [Client, Storage, Operation, Ware, WareOperation]:
+        yield t.query.all()
 
 if __name__ == '__main__':
     #Clear_DB()
-    Print_DB()
+    for x in list(Print_DB()):
+        print(x)
     app.run(debug = True)
--- models.py
+++ models.py
@@ -1,38 +1,62 @@
 from __init__ import db
 from datetime import datetime
 
-ware_operation = db.Table('ware_operation',
-    db.Column('ware_id', db.Integer, db.ForeignKey('ware.id')),
-    db.Column('operation_id', db.Integer, db.ForeignKey('operation.id'))
-                          )
 
-ware_storage = db.Table('ware_storage',
-    db.Column('ware_id', db.Integer, db.ForeignKey('ware.id')),
-    db.Column('storage_id', db.Integer, db.ForeignKey('storage.id'))
-                          )
+class WareOperation(db.Model):
+    ware_id = db.Column(db.Integer, db.ForeignKey('ware.id'), primary_key=True)
+    operation_id = db.Column(db.Integer, db.ForeignKey('operation.id'), primary_key=True)
+    ware_count = db.Column(db.INTEGER, default=1)
+    wares = db.relationship("Ware", back_populates='op_ware')
+    operations = db.relationship("Operation", back_populates='op_ware')
+
+    def __repr__(self):
+        return "Ware_id: {}, Operation_id: {}, Ware_count: {}".format(self.ware_id, self.operation_id, self.ware_count)
+
+
+class WareStorage(db.Model):
+    ware_id = db.Column(db.Integer, db.ForeignKey('ware.id'), primary_key=True)
+    storage_id = db.Column(db.Integer, db.ForeignKey('storage.id'), primary_key=True)
+    ware_count = db.Column(db.INTEGER, default=1)
+    wares = db.relationship("Ware", back_populates='st_ware')
+    storages = db.relationship("Storage", back_populates='st_ware')
+
+    def __repr__(self):
+        return "Ware_id: {}, Storage_id: {}, Ware_count: {}".format(self.ware_id, self.storage_id, self.ware_count)
+
+# ware_operation = db.Table('ware_operation',
+#                           db.Column('ware_id', db.Integer, db.ForeignKey('ware.id')),
+#                           db.Column('operation_id', db.Integer, db.ForeignKey('operation.id'))
+#                           )
+
+
+# ware_storage = db.Table('ware_storage',
+#                         db.Column('ware_id', db.Integer, db.ForeignKey('ware.id')),
+#                         db.Column('storage_id', db.Integer, db.ForeignKey('storage.id'))
+#                         )
 
 
 class Storage(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     phone_number = db.Column(db.String(16), index=True, unique=True)
     address = db.Column(db.String(256), index=True, unique=True)
-    storageble_wares = db.relationship("Ware", secondary=ware_storage, backref="storage")
+
+    st_ware = db.relationship("WareStorage", back_populates="storages")
 
     def __repr__(self):
-        return "Storage id {}".format(self.name)
+        return "Storage {}, St_Ware: {}".format(self.address, self.st_ware)
 
 
 class Operation(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     id_Client = db.Column(db.Integer, db.ForeignKey('client.id'), index=True)
-    ware_count = db.Column(db.INTEGER, default=1)
+    client = db.relationship('Client', back_populates='operations')
     operation_type = db.Column(db.String(16), index=True)
     date_time = db.Column(db.TIMESTAMP, default=datetime.now())
-    operated_wares = db.relationship("Ware", secondary=ware_operation, backref="operation")
-    client_id = db.Column(db.Integer, db.ForeignKey('Client.id'))
+
+    op_ware = db.relationship("WareOperation", back_populates='operations')
 
     def __repr__(self):
-        return "Operation {}".format(self.name)
+        return "Operation {}, Op_Ware: {}".format(self.operation_type, self.op_ware)
 
 
 class Ware(db.Model):
@@ -40,14 +64,17 @@ class Ware(db.Model):
     name = db.Column(db.String(512), index=True)
     price = db.Column(db.FLOAT, index=True)
 
+    st_ware = db.relationship("WareStorage", back_populates="wares")
+    op_ware = db.relationship("WareOperation", back_populates='wares')
+
     def __repr__(self):
-        return "Ware {}".format(self.name)
+        return "Ware {}, Op_Ware: {}, St_Ware: {}".format(self.name, self.op_ware, self.st_ware)
 
 
 class Client(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(256), index=True)
-    operations = db.relationship('Operation', backref='client')
+    operations = db.relationship('Operation', back_populates='client')
 
     def __repr__(self):
-        return "Client {}".format(self.name)
+        return "Client {}, {}".format(self.name, self.operations)
